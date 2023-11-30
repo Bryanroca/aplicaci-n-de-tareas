953476aadb555dce681345b5555d907bed36e94e
Bryan roca <braynroca@gmail.com>
2023-11-30 11:19:47 -0300

    actualizar_tareas

--- frontend/src/api/events.api.js
+++ frontend/src/api/events.api.js
@@ -3,5 +3,7 @@ import axios from 'axios';
 const eventsApi = axios.create({ baseURL: 'http://localhost:8000/event/api/v1/event/' });
 
 export const getAllEvents = () => eventsApi.get('/');
+export const getEvent = (id) => eventsApi.get(`/${id}/`)
 export const createEvent = (event) => eventsApi.post('/', event);
 export const DeleteEvent = (id) => eventsApi.delete(`/${id}`)
+export const updateEvent = (id, event) => eventsApi.put(`/${id}/`, event)
--- frontend/src/pages/EventFormPage.jsx
+++ frontend/src/pages/EventFormPage.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { createEvent, DeleteEvent } from '../api/events.api';
+import { createEvent, DeleteEvent, updateEvent, getEvent } from '../api/events.api';
 import { format } from 'date-fns';
 import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react';
 
 export function EventFormPage() {
     const {
         register,
         handleSubmit,
         formState: { errors },
+        setValue
     } = useForm();
 
     const navigate = useNavigate();
     const params = useParams();
 
+
     const onSubmit = handleSubmit(async (data) => {
-        data.date = format(new Date(data.date), 'yyyy-MM-dd');
-        const res = await createEvent(data);
-        console.log(data)
-        navigate("/events")
+        if (params.id) {
+            await updateEvent(params.id, data)
+            navigate("/events")
+
+        } else {
+
+            data.date = format(new Date(data.date), 'yyyy-MM-dd');
+            const res = await createEvent(data);
+            navigate("/events")
+        }
+
+
     });
+    useEffect(() => {
+        async function loadEvent() {
+
+            if (params.id) {
+                const res = await getEvent(params.id)
+                setValue('title', res.data.title)
+                setValue('description', res.data.description)
+                setValue('date', res.data.date)
+            }
+        }
+        loadEvent()
+    }, [])
 
     return (
         <div>
